ed71167b898d6d37bd800142a6802b005c7b6b5d
hwangjeyeon <kingpele9018@gmail.com>
2023-11-12 00:49:15 +0900

    Fix: OrderInfoSave 추가
    - OrderInfoSave 객체 추가

--- src/main/java/christmas/DomainEntityManager.java
+++ src/main/java/christmas/DomainEntityManager.java
@@ -3,17 +3,23 @@ package christmas;
 import christmas.domain.OrderInfo;
 import christmas.domain.User.UserOrderInfo;
 import christmas.domain.User.UserVisitDay;
+import christmas.domain.menu.MenuInfo;
+
+import java.util.EnumMap;
+
 
 public class DomainEntityManager {
 
     private final UserOrderInfo userOrderInfo;
     private final UserVisitDay userVisitDay;
     private final OrderInfo orderInfo;
+    private final EnumMap<MenuInfo, Integer> orderPrice;
 
     public DomainEntityManager(UserVisitDay userVisitDay, UserOrderInfo userOrderInfo) {
         this.userVisitDay = userVisitDay;
         this.userOrderInfo = userOrderInfo;
         this.orderInfo = new OrderInfo();
+        this.orderPrice = new EnumMap<>(MenuInfo.class);
     }
 
     public UserOrderInfo getUserOrderInfo() {
@@ -26,5 +32,9 @@ public class DomainEntityManager {
 
     public OrderInfo getOrderInfo() {
         return orderInfo;
+    }
+
+    public EnumMap<MenuInfo, Integer> getOrderPrice() {
+        return orderPrice;
     }
 }
