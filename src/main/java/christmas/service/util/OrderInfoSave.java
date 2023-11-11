6b7fdfd897fd2b0dc8f516e87d8f1b14fdb26f27
hwangjeyeon <kingpele9018@gmail.com>
2023-11-12 00:48:25 +0900

    Feat: OrderInfoSave 추가
    - OrderInfoSave 사용자가 주문한 메뉴 * 가격해서 지불해야할 총 금액을 저장하는 기능

--- src/main/java/christmas/service/util/OrderInfoSave.java
+++ src/main/java/christmas/service/util/OrderInfoSave.java
@@ -1,8 +1,10 @@
 package christmas.service.util;
 
 import christmas.DomainEntityManager;
+import christmas.domain.menu.MenuInfo;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class OrderInfoSave {
 
@@ -13,9 +15,14 @@ public class OrderInfoSave {
     }
 
     public void calculateOrderAmount(){
-        List<String> orderCounts = domainEntityManager.getUserOrderInfo().getMenu();
+        List<String> orderMenu = domainEntityManager.getUserOrderInfo().getMenu();
         //TODO 메뉴별 가격 와야함
+        int totalAmount = orderMenu.stream()
+                .map(MenuInfo::valueOf)
+                .mapToInt(menuInfo ->  domainEntityManager.getOrderPrice().get(menuInfo))
+                .sum();
 
+        domainEntityManager.getOrderInfo().setBeforeOrderAmount(totalAmount);
     }
 
 }
