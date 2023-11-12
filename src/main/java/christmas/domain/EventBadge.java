c0c2aa9d48b65b3b9e1e3b42a94132d140e34838
hwangjeyeon <kingpele9018@gmail.com>
2023-11-13 01:16:18 +0900

    feat: EventBadge 도메인 추가
    - 12월 이벤트 배지 정보를 담고 있는 도메인

---
+++ src/main/java/christmas/domain/EventBadge.java
@@ -0,0 +1,15 @@
+package christmas.domain;
+
+public class EventBadge {
+
+    private final String eventBadge;
+
+    public EventBadge(String eventBadge) {
+        this.eventBadge = eventBadge;
+    }
+
+
+    public String getEventBadge() {
+        return eventBadge;
+    }
+}
