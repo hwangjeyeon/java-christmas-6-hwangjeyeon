63ef41a962ddeebe18104d21f1b1c464e08c4c07
hwangjeyeon <kingpele9018@gmail.com>
2023-11-13 17:16:26 +0900

    fix: UserBenefitInfo 수정
    - DDayCheck 클래스 추가에 따른 메소드 추가

--- src/main/java/christmas/domain/User/UserBenefitInfo.java
+++ src/main/java/christmas/domain/User/UserBenefitInfo.java
@@ -1,21 +1,32 @@
 package christmas.domain.User;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserBenefitInfo {
 
     private List<String> benefitsName;
-    private List<String> benefitsAmounts;
+    private List<Integer> benefitsAmounts;
 
     private int totalBenefitsAmounts;
     private int afterDiscountAmounts;
 
-
-    public UserBenefitInfo(List<String> benefitsName, List<String> benefitsAmounts) {
-        this.benefitsName = benefitsName;
-        this.benefitsAmounts = benefitsAmounts;
+    public UserBenefitInfo(){
+        benefitsName = new ArrayList<>();
+        benefitsAmounts = new ArrayList<>();
     }
 
 
+    public void addBenefitsListInfo(String benefitsName, int benefitsAmounts){
+        this.benefitsName.add(benefitsName);
+        this.benefitsAmounts.add(benefitsAmounts);
+    }
 
+    public List<String> getBenefitsName() {
+        return benefitsName;
+    }
+
+    public List<Integer> getBenefitsAmounts() {
+        return benefitsAmounts;
+    }
 }
