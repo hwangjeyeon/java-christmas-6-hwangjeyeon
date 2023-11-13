1c998f16b37afe4b0ce4ea2ec33f9632ceb5fb3d
hwangjeyeon <kingpele9018@gmail.com>
2023-11-13 17:10:46 +0900

    feat: DDayCheck 클래스 추가
    - 크리스마스 디데이 할인 여부를 판단하는 로직

---
+++ src/main/java/christmas/service/util/DDayCheck.java
@@ -0,0 +1,24 @@
+package christmas.service.util;
+
+import christmas.DomainEntityManager;
+
+public class DDayCheck {
+    DomainEntityManager domainEntityManager;
+
+    public DDayCheck(DomainEntityManager domainEntityManager) {
+        this.domainEntityManager = domainEntityManager;
+    }
+
+    public void dDayCheck(){
+        if(domainEntityManager.getUserVisitDay().getVisitDay() <= 25 ){
+            int discounts = 1000 + (domainEntityManager.getUserVisitDay().getVisitDay() - 1) * 100;
+            domainEntityManager.getBenefitInfo().addBenefitsListInfo("크리스마스 디데이 할인", discounts);
+        }
+
+        if(domainEntityManager.getUserVisitDay().getVisitDay() > 25 ){
+            domainEntityManager.getBenefitInfo().addBenefitsListInfo("없음",0);
+        }
+    }
+
+
+}
