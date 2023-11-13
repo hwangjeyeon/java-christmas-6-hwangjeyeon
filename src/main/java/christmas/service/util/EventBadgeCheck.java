0a4c9c7d5050629b6e339c83810a6f6f22d95870
hwangjeyeon <kingpele9018@gmail.com>
2023-11-13 22:45:24 +0900

    feat: EventBadgeCheck 추가
    - 이벤트 배지 정보를 판단하여 저장하는 로직

---
+++ src/main/java/christmas/service/util/EventBadgeCheck.java
@@ -0,0 +1,27 @@
+package christmas.service.util;
+
+import christmas.DomainEntityManager;
+
+public class EventBadgeCheck {
+    private final DomainEntityManager domainEntityManager;
+
+    public EventBadgeCheck(DomainEntityManager domainEntityManager) {
+        this.domainEntityManager = domainEntityManager;
+    }
+
+    public void eventBadgeCheck(){
+
+        if(domainEntityManager.getBenefitInfo().getTotalBenefitsAmounts() >= 5000){
+            domainEntityManager.getEventBadge().setEventBadge("별");
+        }
+
+        if(domainEntityManager.getBenefitInfo().getTotalBenefitsAmounts() >= 10000){
+            domainEntityManager.getEventBadge().setEventBadge("트리");
+        }
+
+        if(domainEntityManager.getBenefitInfo().getTotalBenefitsAmounts() >= 20000){
+            domainEntityManager.getEventBadge().setEventBadge("산타");
+        }
+    }
+
+}
