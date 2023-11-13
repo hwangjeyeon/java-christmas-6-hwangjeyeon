9668e4630431f6f553681ac03ac32cbb7a0db922
hwangjeyeon <kingpele9018@gmail.com>
2023-11-13 21:23:37 +0900

    feat: WeekCategoryCheck 추가
    - 평일/주말 할인 여부 확인 후, 혜택 금액 판단해서 저장하는 로직

---
+++ src/main/java/christmas/service/util/WeekCategoryCheck.java
@@ -0,0 +1,61 @@
+package christmas.service.util;
+
+import christmas.DomainEntityManager;
+import christmas.domain.calendar.December;
+import christmas.domain.menu.Category;
+import christmas.domain.menu.MenuInfo;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class WeekCategoryCheck {
+
+    private final DomainEntityManager domainEntityManager;
+
+    public WeekCategoryCheck(DomainEntityManager domainEntityManager) {
+        this.domainEntityManager = domainEntityManager;
+    }
+
+    public void weekCategoryCheck(){
+        List<String> orderMenu = domainEntityManager.getUserOrderInfo().getMenu();
+        List<Integer> orderCounts = domainEntityManager.getUserOrderInfo().getCounts();
+        String weekCategory = December.getDayOfWeekByDay(domainEntityManager.getUserVisitDay().getVisitDay());
+        if(weekCategory.equals("평일")){
+            menuCategoryInWeekDayCheck(orderMenu, orderCounts);
+        }
+
+        if(weekCategory.equals("주말")){
+            menuCategoryInWeekendDayCheck(orderMenu, orderCounts);
+        }
+    }
+
+
+    public void menuCategoryInWeekDayCheck(List<String> orderMenu, List<Integer> orderCounts){
+        int discounts = IntStream.range(0, orderMenu.size())
+                    .filter(i -> {
+                        String menuName = orderMenu.get(i);
+                        Category category = MenuInfo.getCategoryByName(menuName);
+                        return category == Category.DESSERT; // 원하는 카테고리로 변경
+                    })
+                    .mapToObj(orderCounts::get)
+                    .mapToInt(Integer::intValue)
+                    .sum();
+        domainEntityManager.getBenefitInfo().addBenefitsListInfo("평일 할인", discounts*2023);
+    }
+
+
+    public void menuCategoryInWeekendDayCheck(List<String> orderMenu, List<Integer> orderCounts){
+        int discounts = IntStream.range(0, orderMenu.size()).filter(i -> {
+                    String menuName = orderMenu.get(i);
+                    Category category = MenuInfo.getCategoryByName(menuName);
+                    return category == Category.MAIN; // 원하는 카테고리로 변경
+                })
+                .mapToObj(orderCounts::get)
+                .mapToInt(Integer::intValue)
+                .sum();
+        domainEntityManager.getBenefitInfo().addBenefitsListInfo("주말 할인", discounts*2023);
+    }
+
+
+
+}
