f72cb9cfc3cf327787810df601fc73623e3c9834
hwangjeyeon <kingpele9018@gmail.com>
2023-11-13 22:43:05 +0900

    feat: StarCheck 클래스 추가
    - 별표가 있는 날짜인지 확인하는 로직

---
+++ src/main/java/christmas/service/util/StarCheck.java
@@ -0,0 +1,23 @@
+package christmas.service.util;
+
+import christmas.DomainEntityManager;
+import christmas.domain.calendar.December;
+
+public class StarCheck {
+    private final DomainEntityManager domainEntityManager;
+
+    public StarCheck(DomainEntityManager domainEntityManager) {
+        this.domainEntityManager = domainEntityManager;
+    }
+    
+    public void starCheck(){
+        if(December.getStarByDay(domainEntityManager.getUserVisitDay().getVisitDay())){
+          domainEntityManager.getBenefitInfo().addBenefitsListInfo("특별 할인", 1000);  
+        }
+        
+        if(!December.getStarByDay(domainEntityManager.getUserVisitDay().getVisitDay())){
+            domainEntityManager.getBenefitInfo().addBenefitsListInfo("없음", 0);
+        }
+    }
+    
+}
