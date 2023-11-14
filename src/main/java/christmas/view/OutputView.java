7e58803c989985fb33eeadc4e9c9216f21f17430
hwangjeyeon <kingpele9018@gmail.com>
2023-11-14 16:40:11 +0900

    fix: OutputView 수정
    - OutputService 추가에 따른 OutputView 수정

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,11 +1,12 @@
 package christmas.view;
 
-import christmas.DomainEntityManager;
+
+import christmas.service.OutputService;
 
 import java.util.stream.IntStream;
 
 public class OutputView {
-    DomainEntityManager domainEntityManager;
+    OutputService outputService;
     private final String ORDER_MENU = "\n<주문 메뉴>\n";
     private final String BEFORE_AMOUNTS = "\n<할인 전 총주문 금액>\n";
     private final String GIFT_MENU = "\n<증정 메뉴>\n";
@@ -13,10 +14,10 @@ public class OutputView {
     private final String TOTAL_BENEFITS_AMOUNTS = "\n<총혜택 금액>\n";
     private final String AFTER_DISCOUNTS_AMOUNTS = "\n<할인 후 예상 결제 금액>\n";
     private final String EVENT_BADGE = "\n<12월 이벤트 배지>\n";
-    public OutputView(DomainEntityManager domainEntityManager) {
-        this.domainEntityManager = domainEntityManager;
-    }
 
+    public OutputView(OutputService outputService) {
+        this.outputService = outputService;
+    }
 
     public void printEventBenefitsList(){
         printList();
@@ -30,52 +31,37 @@ public class OutputView {
     }
 
     public void printList(){
-        System.out.printf("12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n"
-                ,domainEntityManager.getUserVisitDay().getVisitDay());
+        System.out.print(outputService.makeStringEventList());
     }
 
     public void printOrderMenu(){
-        System.out.print(ORDER_MENU);
-        IntStream.range(0, domainEntityManager.getUserOrderInfo().getMenu().size())
-                .forEach(i -> {
-                    String menu = domainEntityManager.getUserOrderInfo().getMenu().get(i);
-                    int counts = domainEntityManager.getUserOrderInfo().getCounts().get(i);
-                    System.out.printf("%s %d개\n", menu, counts);
-                });
+        System.out.print(ORDER_MENU + outputService.makeOrderMenuList());
+
     }
 
     public void printBeforeAmounts(){
-        System.out.println(BEFORE_AMOUNTS + domainEntityManager.getOrderInfo().getBeforeOrderAmount() + "개");
+        System.out.print(BEFORE_AMOUNTS + outputService.makeBeforeDiscounts());
     }
 
     public void printGiftMenu(){
-        System.out.println(GIFT_MENU + domainEntityManager.getGiftMenuInfo().getName()
-                + domainEntityManager.getGiftMenuInfo().getCounts() + "개");
+        System.out.print(GIFT_MENU + outputService.makeGiftMenu());
     }
 
     public void printBenefitsList(){
-        System.out.print(BENEFITS_LIST);
-        IntStream.range(0, domainEntityManager.getBenefitInfo().getBenefitsName().size())
-                .forEach(i -> {
-                    String benefitsName = domainEntityManager.getBenefitInfo().getBenefitsName().get(i);
-                    int benefitsAmount = domainEntityManager.getBenefitInfo().getBenefitsAmounts().get(i);
-                    System.out.printf("%s: -%d원\n", benefitsName, benefitsAmount);
-                });
+        System.out.print(BENEFITS_LIST + outputService.makeBenefitsList());
     }
 
     public void printTotalBenefits(){
-        System.out.println(TOTAL_BENEFITS_AMOUNTS + String.format("-%d원"
-                ,domainEntityManager.getBenefitInfo().getTotalBenefitsAmounts()));
+        System.out.print(TOTAL_BENEFITS_AMOUNTS + outputService.makeTotalBenefits());
     }
 
     public void printAfterDiscountsAmounts(){
-        System.out.println(AFTER_DISCOUNTS_AMOUNTS + String.format("%d원"
-                ,domainEntityManager.getBenefitInfo().getAfterDiscountAmounts()));
+        System.out.print(AFTER_DISCOUNTS_AMOUNTS + outputService.makeAfterBenefits());
 
     }
 
     public void printEventBadge(){
-        System.out.println(EVENT_BADGE + domainEntityManager.getEventBadge().getEventBadge());
+        System.out.println(EVENT_BADGE + outputService.makeBadge());
     }
 
 }
